7ca4d7eb2b0b1415130cdfaeb67ec9df8cc02ab9
Kristoffer <kristoffer@kritix.dk>
2021-10-18 18:12:27 +0200

    Skrevet nogle regler op

--- app/src/main/java/com/example/s205354_lykkehjulet/Fragment1.kt
+++
@@ -1,6 +0,0 @@
-package com.example.s205354_lykkehjulet
-
-import androidx.fragment.app.Fragment
-
-class Fragment1 : Fragment(R.layout.fragment1) {
-}
--- app/src/main/java/com/example/s205354_lykkehjulet/Fragment2.kt
+++
@@ -1,6 +0,0 @@
-package com.example.s205354_lykkehjulet
-
-import androidx.fragment.app.Fragment
-
-class Fragment2 : Fragment(R.layout.fragment2) {
-}
