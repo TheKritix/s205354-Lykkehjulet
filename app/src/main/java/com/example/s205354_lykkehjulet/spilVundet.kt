01388eb93eae8448695f81dcbd88a83461542146
Kristoffer <kristoffer@kritix.dk>
2021-11-19 10:27:14 +0100

    Fikset Keyboard.

--- app/src/main/java/com/example/s205354_lykkehjulet/spilVundet.kt
+++ app/src/main/java/com/example/s205354_lykkehjulet/spilVundet.kt
@@ -6,35 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
-import com.example.s205354_lykkehjulet.databinding.FragmentSpilTabtBinding
 import com.example.s205354_lykkehjulet.databinding.FragmentSpilVundetBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [spilVundet.newInstance] factory method to
- * create an instance of this fragment.
- */
 class spilVundet : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private var _binding: FragmentSpilVundetBinding? = null
 
     private val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,29 +27,8 @@ class spilVundet : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         binding.spilIgenKnap.setOnClickListener{
             Navigation.findNavController(it).navigate(spilVundetDirections.actionSpilVundetToLykkehjulSpil())
         }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment spilVundet.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            spilVundet().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
